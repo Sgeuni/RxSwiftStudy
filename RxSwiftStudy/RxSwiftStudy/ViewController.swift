b123b9b1e11110ae5689be7718b05a0bb47e2983
geuni <geuni0110@gmail.com>
2018-01-08 13:07:39 +0900

    Observable

--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -13,35 +13,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.stringSequence()
-        self.intArraySequence()
-        self.dicSequence()
+//        self.stringSequence()
+//        self.intArraySequence()
+//        self.dicSequence()
+        
+        rxTestObservable(element: [1,2,3,4,5]).subscribe({ (n) in
+            print(n)
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func stringSequence() {
-        let stringSequence = Observable.just("test string")
-        stringSequence.subscribe { (event: Event<String>) in
-            print(event)
-        }
+    func rxTestObservable<E>(element: E) -> Observable<E> {
+        return Observable.create({ observer -> Disposable in
+            observer.on(.next(element))
+            observer.on(.completed)
+            return Disposables.create()
+        })
     }
     
-    func intArraySequence() {
-        let arraySequence = Observable.from([1,2,3,4,5])
-        arraySequence.subscribe { (event: Event<Int>) in
-            print(event)
-        }
-    }
-    
-    func dicSequence() {
-        let dicSequence = Observable.from([1:"Rx", 2:"Swift"])
-        dicSequence.subscribe { (event: Event<(key:Int, value:String)>) in
-            print(event)
-        }
-    }
+//    func stringSequence() {
+//        let stringSequence = Observable.just("test string")
+//        stringSequence.subscribe { (event: Event<String>) in
+//            print(event)
+//        }
+//    }
+//
+//    func intArraySequence() {
+//        let arraySequence = Observable.from([1,2,3,4,5])
+//        arraySequence.subscribe { (event: Event<Int>) in
+//            print(event)
+//        }
+//    }
+//
+//    func dicSequence() {
+//        let dicSequence = Observable.from([1:"Rx", 2:"Swift"])
+//        dicSequence.subscribe { (event: Event<(key:Int, value:String)>) in
+//            print(event)
+//        }
+//    }
 
 }
 
